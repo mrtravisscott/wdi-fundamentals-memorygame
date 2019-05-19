5937c2ce8096b4c2a3484e75b574ec3304b22f60
travis scott <travisscott@traviss-Air.lan>
2019-05-19 16:04:29 -0700

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var CardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
