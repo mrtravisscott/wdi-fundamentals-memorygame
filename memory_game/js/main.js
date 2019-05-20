1c371a92fed56514f42cc4e564c1201ce5de77ca
travis scott <travisscott@traviss-MacBook-Air.local>
2019-05-19 21:44:10 -0700

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,8 @@
 console.log("Up and running!");
-var cards = ["queen", "king", "queen", "king"];
+var cards = [{"rank": "queen", "suit": "hearts", "cardImage" : "images/queen-of-hearts.png"}, 
+{"rank": "queen", "suit": "diamonds", "cardImage" : "images/queen-of-diamonds.png"},
+{"rank": "king", "suit": "hearts", "cardImage" : "images/king-of-hearts.png"},
+{"rank": "king", "suit": "diamonds", "cardImage" : "images/king-of-diamonds.png"}];
 var cardsInPlay = [];
 
 function checkForMatch(){
@@ -10,8 +13,10 @@ function checkForMatch(){
 		}
 }
 function flipCard(cardId){
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
