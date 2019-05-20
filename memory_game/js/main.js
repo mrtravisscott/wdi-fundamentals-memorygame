5321b4c84efae45ea79c29b16b42dcee56249ae1
travis scott <travisscott@traviss-MacBook-Air.local>
2019-05-19 19:07:00 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var CardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("user flipped queen");
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("user flipped king");
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
